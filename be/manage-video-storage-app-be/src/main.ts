cd0e8d3cb54f142c58ab6b60692677efe87d26ac
thanhlam212 <nguyenthanhlam02122003@gmail.com>
2024-06-13 14:48:05 +0700

    fix CORS

--- be/manage-video-storage-app-be/src/main.ts
+++ be/manage-video-storage-app-be/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4000', // URL của frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
